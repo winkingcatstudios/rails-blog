566fcfb868b1e26a3ba0216c377bce559cfc5ea5
winkingcat <dankercher@gmail.com>
2022-09-23 16:54:04 -0400

    added create article from form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,13 @@ class ArticlesController < ActionController::Base
   def index
     @articles = Article.all
   end
+
+  def new
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    redirect_to article_path(@article)  # 'redirect_to @article' works too
+  end
 end
