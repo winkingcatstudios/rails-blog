4687d6503aa66330a6d5c5e2b50b932a8053a90d
winkingcat <dankercher@gmail.com>
2022-09-26 09:30:20 -0400

    added delete functions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -33,4 +33,10 @@ class ArticlesController < ApplicationController
       render :edit, status: :unprocessable_entity
     end
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
 end
