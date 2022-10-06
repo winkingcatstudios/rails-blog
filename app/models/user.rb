4359e91cc6e50f7be2b58e3142ac1a5cec9cafd6
Dan Kercher <dkercher@iu.edu>
2022-10-06 08:53:20 -0400

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  validates :username, presence: true,
+                       uniqueness: { case_sensative: false },
+                       length: { minimum: 3, maximum: 25 }
+  validates :email, presence: true,
+                    uniqueness: { case_sensative: false },
+                    length: { minimum: 3, maximum: 254 },
+                    # Verify email with ruby's standard library regex.
+                    format: { with: URI::MailTo::EMAIL_REGEXP }
 end
