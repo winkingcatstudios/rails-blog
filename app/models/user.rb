bb5922bad299df375e7604c029cd242438feccf8
Dan Kercher <dkercher@iu.edu>
2022-10-06 10:36:54 -0400

    display user in article list

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+
   has_many :articles
+
   validates :username, presence: true,
-                       uniqueness: { case_sensative: false },
+                       uniqueness: { case_sensitive: false },
                        length: { minimum: 3, maximum: 25 }
   validates :email, presence: true,
-                    uniqueness: { case_sensative: false },
+                    uniqueness: { case_sensitive: false },
                     length: { minimum: 3, maximum: 254 },
                     # Verify email with ruby's standard library regex.
                     format: { with: URI::MailTo::EMAIL_REGEXP }
