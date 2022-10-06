e54c2ba65df9b24dbb3ccedb59dddbcf772864a0
Dan Kercher <dkercher@iu.edu>
2022-10-06 14:23:07 -0400

    Added user edit feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:destroy, :edit, :show, :update]
+
   def new
     @user = User.new
   end
@@ -11,9 +13,24 @@ class UsersController < ApplicationController
       render "new", status: :unprocessable_entity
     end
   end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to articles_path, notice: "Your account was updated successfully"
+    else
+      render "edit", status: :unprocessable_entity
+    end
+  end
 end
 
 private
+
+def set_user
+  @user = User.find(params[:id])
+end
 
 def user_params
   params.require(:user).permit(:username, :email, :password)
